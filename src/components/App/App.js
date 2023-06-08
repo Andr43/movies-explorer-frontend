4f7c7f12b7aa5c79dc75c8fbf513ecf43fb01f54
Andrey Musinov <andrew.musinov@gmail.com>
2023-06-08 15:04:47 +0100

    Подготовил маршруты проекта

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Routes>
+      <Route path="/" element={''} />
+      <Route path="/movies" element={''} />
+      <Route path="/saved-movies" element={''} />
+      <Route path="/profile" element={''} />
+      <Route path="/signin" element={''} />
+      <Route path="/signup" element={''} />
+      </Routes>
+      </>
+  );
+}
+
+export default App;
